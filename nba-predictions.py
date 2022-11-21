3101137ca1302b52ea0f1d382e33f56d57da027d
Javier prieto <javiprietod@Javiers-MacBook-Pro.local>
2022-11-21 09:38:19 +0100

    main

--- nba-predictions.py
+++ nba-predictions.py
@@ -1,1 +1,16 @@
 import requests
+
+
+
+def extract(team):
+    api_key = eval(open('config.txt','r').read())['auth']
+    response = requests.get(f'https://api.sportsdata.io/v3/nba/scores/json/AllTeams?key={api_key}').json()
+    teams = {}
+    for team in response:
+        if team['Active'] == 'true':
+            print('a')
+            teams[team['City'] +' '+ team['Name']] = (team['TeamID'],team['Key'])
+    team_key = teams[team][1]
+    player_stats = requests.get(f'https://api.sportsdata.io/v3/nba/stats/json/PlayerSeasonStatsByTeam/2022/{team_key}?key={api_key}').json()
+    
+
